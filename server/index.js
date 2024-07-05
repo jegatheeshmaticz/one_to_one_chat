d89311a225f0341a96b255d69363cb7779c90ac2
jegatheeshmaticz <jegatheeswaranmaticz@gmail.com>
2024-07-05 12:58:30 +0530

    new file uploaded

---
+++ server/index.js
@@ -0,0 +1,80 @@
+// const express = require("express");
+// const app = express();
+// const http = require("http");
+// const cors = require("cors");
+// const { Server } = require("socket.io");
+// app.use(cors());
+
+// const server = http.createServer(app);
+
+// const io = new Server(server, {
+//   cors: {
+//     origin: "http://localhost:3000",
+//     methods: ["GET", "POST"],
+//   },
+// });
+
+// io.on("connection", (socket) => {
+//   console.log(`User Connected: ${socket.id}`);
+
+//   socket.on("join_room", (data) => {
+//     socket.join(data);
+//     console.log(`User with ID: ${socket.id} joined room: ${data}`);
+//   });
+
+//   socket.on("send_message", (data) => {
+//     socket.to(data.room).emit("receive_message", data);
+//   });
+
+//   socket.on("disconnect", () => {
+//     console.log("User Disconnected", socket.id);
+//   });
+// });
+
+// server.listen(3001, () => {
+//   console.log("SERVER RUNNING");
+// });
+
+
+
+const express = require("express");
+const app = express();
+const http = require("http");
+const cors = require("cors");
+const { Server } = require("socket.io");
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+    credentials: true  // Allow credentials (cookies, authorization headers) to be sent
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log(`User Connected: ${socket.id}`);
+
+  socket.on("join_room", (data) => {
+    socket.join(data);
+    console.log(`User with ID: ${socket.id} joined room: ${data}`);
+  });
+
+  socket.on("send_message", (data) => {
+    socket.to(data.room).emit("receive_message", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User Disconnected", socket.id);
+  });
+});
+
+app.use(cors({
+  origin: "http://localhost:3000",
+  credentials: true  // Allow credentials (cookies, authorization headers) to be sent
+}));
+
+server.listen(3001, () => {
+  console.log("SERVER RUNNING");
+});
